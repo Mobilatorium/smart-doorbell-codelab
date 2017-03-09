881da8b50c1e504542d1f7b7912eca500cb28a3a
Eugene Komarov <eugenek@handsome.is>
2017-03-09 18:36:47 +0600

    Add companionApp

---
+++ companionApp/src/main/java/is/handsome/labs/doorbellcodelab/MainActivity.java
@@ -0,0 +1,62 @@
+package is.handsome.labs.doorbellcodelab;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private DatabaseReference mDatabaseRef;
+
+    private RecyclerView mRecyclerView;
+    private DoorbellEntryAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.doorbellView);
+        // Show most recent items at the top
+        LinearLayoutManager layoutManager =
+                new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, true);
+        mRecyclerView.setLayoutManager(layoutManager);
+
+        // Reference for doorbell events from embedded device
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference().child("logs");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        mAdapter = new DoorbellEntryAdapter(this, mDatabaseRef);
+        mRecyclerView.setAdapter(mAdapter);
+
+        // Make sure new events are visible
+        mAdapter.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver() {
+            @Override
+            public void onItemRangeChanged(int positionStart, int itemCount) {
+                mRecyclerView.smoothScrollToPosition(mAdapter.getItemCount());
+            }
+        });
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // Tear down Firebase listeners in adapter
+        if (mAdapter != null) {
+            mAdapter.cleanup();
+            mAdapter = null;
+        }
+    }
+
+}
