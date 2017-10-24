d963171d9ee6c1c8479c3d4aa9e33d1f30b29f2d
Eugene Komarov <opengamer29@gmail.com>
2017-10-24 10:27:52 +0600

    Step # 2 Add button

--- app/src/main/java/is/handsome/labs/doorbellcodelab/IoTActivity.java
+++ app/src/main/java/is/handsome/labs/doorbellcodelab/IoTActivity.java
@@ -3,9 +3,18 @@ package is.handsome.labs.doorbellcodelab;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.google.android.things.contrib.driver.button.Button;
+import com.google.android.things.pio.PeripheralManagerService;
+
+import java.io.IOException;
+
 import timber.log.Timber;
 
 public class IoTActivity extends Activity {
+
+    private static final String GPIO_PIN_BUTTON_NAME = "BCM21";
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,11 +23,33 @@ public class IoTActivity extends Activity {
 
         Timber.plant(new Timber.DebugTree());
 
+        PeripheralManagerService service = new PeripheralManagerService();
+        Timber.d("Available GPIO: " + service.getGpioList());
+
+        try {
+            button = new Button(
+                    GPIO_PIN_BUTTON_NAME,
+                    Button.LogicState.PRESSED_WHEN_LOW);
+            button.setOnButtonEventListener(new Button.OnButtonEventListener() {
+                @Override
+                public void onButtonEvent(Button button, boolean pressed) {
+                    String pressedString = pressed ? "pressed" : "unpressed";
+                    Timber.d(GPIO_PIN_BUTTON_NAME + ": " + pressedString);
+                }
+            });
+        } catch (IOException e) {
+            Timber.e(e);
+        }
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
 
+        try {
+            button.close();
+        } catch (IOException e) {
+            Timber.e(e, "Error while close " + GPIO_PIN_BUTTON_NAME + "button");
+        }
     }
 }
